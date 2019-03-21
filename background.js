b8170102099e7a4b77c122bb05f2beb890fa9325
Danny Summerlin <danny.summerlin@jstart.org>
2019-03-21 15:34:30 -0400

    adding list copying
    copies elements of the list to a TSV
    -- needs to have a button added to lists so it can do it selectively

--- background.js
+++ background.js
@@ -130,3 +130,44 @@ var getIdFromUrl = function(url) {
 	return false
 }
 var cleanUrl = function(url, currentId) { return url.match(/.*force.com/)[0] + "/" + currentId }
+var extractList = (element)=>{
+	let rows = element
+	if(rows == undefined)
+		rows = document.querySelectorAll(".forceSearchResultsGridView tr")
+	let headers = []
+	Object.values(rows[0].children).forEach(e=>{
+		try { headers.push(e.querySelector("a").innerText.replace("SORT\n","")) }
+		catch(er) {}
+	})
+	let exportList = [headers]
+	for (var i = 1; i < rows.length; i++) {
+		let j = 0
+		let item = {}
+		Object.values(rows[i].children).forEach(e=>{
+			try {
+				item[ headers[j] ] = e.innerText
+				j++
+			} catch(er) {}
+		})
+		exportList.push(item)
+	}
+	return exportList
+}
+var copyList = (element)=>{
+// probably generlize finalcopy
+	var cb = makeClipboard()
+	let items = extractList(element)
+	let headers = items[0]
+	let output = headers.join("\t") + "\n"
+	for (var i = 1; i < items.length; i++) {
+		let item = []
+		for (var j = 0; j < headers.length; j++) {
+			item.push(items[i][ headers[j] ])
+		}
+		output += item.join("\t") + "\n"
+	}
+	cb.textContent = output
+	cb.select()
+	document.execCommand('copy')
+	cb.remove()
+}
